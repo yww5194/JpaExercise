4a36aeeaf8af41b97fed1f12f89265fce9b22a0a
yww5194 <yww5194@OZWDLND341-68.win.psu.edu>
2015-10-08 09:20:09 -0400

    added sqljdvc42.jar to Libraries

---
+++ JpaExercise/src/jpaexercise/FXMLDocumentController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseaccess;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author pxo4
+ */
+public class FXMLDocumentController implements Initializable {
+  
+  @FXML
+  private Label label;
+  
+  @FXML
+  private void handleButtonAction(ActionEvent event) {
+    System.out.println("You clicked me!");
+    label.setText("Hello World!");
+  }
+  
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+      
+      EntityManagerFactory emf;
+      emf = persistence.createEntityManagerFactory("JpaExercisePU");
+      
+      Person dude = new Person();
+      dude.setFirstName("Joe");
+      
+      PersonJpaController jpaPerson
+      
+      
+  }  
+  
+}
